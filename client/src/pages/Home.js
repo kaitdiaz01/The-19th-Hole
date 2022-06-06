bddca25102ee6c19fb8a26d325a49823ddd31c40
Kaitlyn Diaz <katediaz011@gmail.com>
2022-06-06 14:53:15 -0400

    trying to change homepage and navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Signup from "../components/Signup";
+// import Signup from "../components/Signup";
 import Login from "../components/Login";
 
 
 const Home = () => {
   return (
     <div className="container">
-     <Signup/>
+     {/* <Signup/> */}
      <Login/> 
     </div>
   );
