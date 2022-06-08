e01866dfdcf7649bd7388d34f438a396288ee50f
nick quinn <nick.quinn@stadium1.com>
2022-06-08 11:27:18 -0400

    added props to the card so we can pass in information.  also wireframed a findplayers page

---
+++ client/src/pages/FindPlayers.js
@@ -0,0 +1,11 @@
+import {React, useState} from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+
+//map over the data
+//return back a card for every user that matches the criteria
+//bring in user card component
+    //data in each part of the user card
+        //data.map pass over the user card
+            //<UserCard name={user.name} level={user.level}/>
