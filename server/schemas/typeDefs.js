b386dfcafebbfe5bd819d7090714d32755955efb
Kaitlyn Diaz <katediaz011@gmail.com>
2022-06-09 10:27:42 -0400

    took out userlogin const

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,7 +26,7 @@ const typeDefs = gql`
   type Mutation {
 
     addUser(username: String!, email: String!, password: String!):Auth
-    updateUser(city: String!, level: String!,drink:Boolean!,smoke:Boolean!): Userlogin
+    updateUser(city: String!, level: String!,drink:Boolean!,smoke:Boolean!): User
     login(email: String!, password: String!):Auth
 
 
